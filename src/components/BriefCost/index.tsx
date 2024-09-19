1557ef54387ba468bfcdabfaa005c64ae610e163
PetrachkovVasiliy <vasyapt04mail.com>
2024-09-19 22:40:00 +0300

    Update index.tsx

--- src/components/BriefCost/index.tsx
+++ src/components/BriefCost/index.tsx
@@ -3,13 +3,15 @@ import { useAppSelector } from "../../hooks/redux";
 function BriefCost() {
   const user = useAppSelector((state) => state.userReducer);
 
+  const briefNums = calculateBrief();
+
   function calculateBrief() {
     let diff = 0;
     let sum = 0;
     let newSum = 0;
 
     user.dataBrief.forEach((item, index) => {
-      if (user.data[index].coin) {
+      if (user.data[index] && user.data[index].coin) {
         diff += Number(user.data[index].coin.priceUsd) - Number(item.priceUsd);
         sum += Number(user.data[index].coin.priceUsd) * user.data[index].amount;
         newSum += Number(item.priceUsd) * user.data[index].amount;
@@ -20,8 +22,9 @@ function BriefCost() {
 
   return (
     <pre>
-      {calculateBrief().sum} <span>USD</span> {calculateBrief().diff}{" "}
-      <span>({calculateBrief().newSum / calculateBrief().sum} %)</span>
+      {Number(briefNums.sum).toFixed(2)} <span>USD</span>{" "}
+      {Number(briefNums.diff).toFixed(2)}{" "}
+      <span>({Number(briefNums.newSum / briefNums.sum).toFixed(2)} %)</span>
     </pre>
   );
 }
